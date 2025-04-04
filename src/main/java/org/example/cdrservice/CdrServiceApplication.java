9e24565d1ca849d1194a4a206a366b7b088f7fea
is6769 <igor.shvets2007@mail.ru>
2025-04-04 13:25:39 +0300

    first commit

---
+++ src/main/java/org/example/cdrservice/CdrServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.cdrservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdrServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdrServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cdrservice/CdrServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cdrservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdrServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
