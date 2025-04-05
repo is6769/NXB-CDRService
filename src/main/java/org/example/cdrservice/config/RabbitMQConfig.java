2036b976fb7dd2f5d3a8d9bfffaaa90030ca4305
is6769 <igor.shvets2007@mail.ru>
2025-04-05 20:48:29 +0300

    liquibase support v1 finished

---
+++ src/main/java/org/example/cdrservice/config/RabbitMQConfig.java
@@ -0,0 +1,8 @@
+package org.example.cdrservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+}
